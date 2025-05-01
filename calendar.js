5f472f61f28403e85ce2681441baa93e222ddbbb
idelaluz <israel.delaluz@gmail.com>
2025-05-01 11:01:04 +0200

    Commit 01.05.2025 11h00

---
+++ calendar.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const monthYear = document.getElementById('month-year');
+
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    
+    let currentDate = new Date();
+    let today = newDate();
+
+    function renderCalendar(date) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstDay = new Date(year, month, 1).getDay();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+
+        monthYear.textContent = `${months[month]} ${year}`;
+
+    }
+
+    renderCalendar(currentDate);
+});
