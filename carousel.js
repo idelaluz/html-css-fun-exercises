2d54c5ea97c4193ba15cba9cff6aab2b7159069c
idelaluz <israel.delaluz@gmail.com>
2025-05-04 14:29:43 +0200

    Commit 04.05.2025 14h29

---
+++ carousel.js
@@ -0,0 +1,10 @@
+const items = [
+    { title: "HTML", unicode: "U+1F433" },
+    { title: "CSS", unicode: "U+1F40B" },
+    { title: "JavaScript", unicode: "U+1F42C" },
+    { title: "React JS", unicode: "U+1F41F" },
+    { title: "Node JS", unicode: "U+1F420" },
+];
+
+const carousel = document.getElementById("carousel");
+
