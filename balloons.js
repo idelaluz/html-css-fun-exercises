9245921ab07c687628742c008b284955811e648b
idelaluz <israel.delaluz@gmail.com>
2025-05-13 00:18:42 +0200

    Commit 13.05.2025 00h18

--- balloons.js
+++ balloons.js
@@ -1,3 +1,8 @@
 const createBalloon = () => {
-    
-}
+    const balloon = document.querySelection('.balloon').cloneNode(true);
+    balloon.style.left = Math.random() * 100 + '%';
+    balloon.style.animationDelay = Math.random() * 1 + 's';
+    document.body.appendChild(balloon);
+};
+
+for (let i = 0; i < 5; )
