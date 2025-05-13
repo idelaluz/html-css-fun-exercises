c68b9ae3b562dbde53dccaa819abff13388ca3f9
idelaluz <israel.delaluz@gmail.com>
2025-05-13 09:27:37 +0200

    Commit 13.05.2025 09h27

--- donut.js
+++ donut.js
@@ -1,5 +1,5 @@
 const donut = document.querySelector(".donut");
-const colors = ['#00e676', '#ffeb3b', '#ff1744', '#2196f3', '#ff9100'];
+const colors = ["#00e676", "#ffeb3b", "#ff1744", "#2196f3", "#ff9100"];
 
 function createSprinkle() {
     const sprinkle = document.createElement("div");
