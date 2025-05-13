3e3c974429dff2b6760c5122cfed0c4c5854e96f
idelaluz <israel.delaluz@gmail.com>
2025-05-13 18:52:00 +0200

    Commit 13.05.2025 18h51

--- donut.js
+++ donut.js
@@ -1,14 +1,13 @@
-const donut = document.querySelector(".donut");
-const colors = ["#00e676", "#ffeb3b", "#ff1744", "#2196f3", "#ff9100"];
+const donut = document.querySelector('.donut');
+const colors = ['#00e676', '#ffeb3b', '#ff1744', '#2196f3', '#ff9100'];
 
 function createSprinkle() {
-    const sprinkle = document.createElement("div");
-    sprinkle.classList.add("sprinkle");
-    sprinkle.style.top = Math.random() * 80 + 30 + "px";
-    sprinkle.style.left = Math.random() * 160 + 20 + "px";
+    const sprinkle = document.createElement('div');
+    sprinkle.classList.add('sprinkle');
+    sprinkle.style.top = Math.random()*80 + 30 + 'px';
+    sprinkle.style.left = Math.random()*160 + 20 + 'px';
     sprinkle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
     donut.appendChild(sprinkle);
-
 
     setTimeout(() => {
         sprinkle.remove();
